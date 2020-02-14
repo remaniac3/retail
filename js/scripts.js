540b930ab2e8902e86014c67730c67e03d361840
R3MANIAC <50291272+remaniac3@users.noreply.github.com>
2020-02-14 11:58:49 +0800

    daily progress
    more like yesterday's progress

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function reviewCustCToggle() {
+	let viewportWidth = window.innerWidth;
+
+	// if (viewportWidth <= 420) {
+	// 	if (document.getElementById("reviewCustC").classList.contains('speechBoxTriA')) {
+	// 		document.getElementById("reviewCustC").classList.add('speechBoxTriC');
+	// 		document.getElementById("reviewCustC").classList.remove('speechBoxTriA');
+	// 	}
+	// }
+
+	console.log(viewportWidth);
+}
